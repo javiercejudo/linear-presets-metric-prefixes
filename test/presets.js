1eeb9d278a678083f5a9ec0b316392919564e886
Javier Cejudo <javier@javiercejudo.com>
2015-09-01 01:38:16 +1000

    feat: modify presets schema

--- test/presets.js
+++ test/presets.js
@@ -4,11 +4,12 @@
 
 require('should');
 
-var rescale = require('rescale')(require('floating-adapter')).rescale;
-var metric = require('../src/linear-presets-metric-prefixes');
+var Decimal = require('linear-arbitrary-precision')(require('floating-adapter'));
+var rescale = require('rescale')(Decimal).rescale;
+var metric = require('linear-preset-factory')(require('../src/linear-presets-metric-prefixes'));
 
 function convert(x, preset) {
-  return rescale(x, preset[0], preset[1]);
+  return Number(rescale(x, preset[0], preset[1]));
 };
 
 function invert(preset) {
@@ -17,45 +18,46 @@ function invert(preset) {
 
 describe('metric prefixes presets', function() {
   it('should convert correctly', function() {
-    (1).should.be.approximately(convert(1e-24, invert(metric.noneToYotta)), 1e-15, 'noneToYotta')
-      .and.approximately(convert(1e-21, invert(metric.noneToZetta)), 1e-15, 'noneToZetta')
-      .and.exactly(convert(1e-15, invert(metric.noneToPeta)), 'noneToPeta')
-      .and.exactly(convert(1e-12, invert(metric.noneToTera)), 'noneToTera')
-      .and.exactly(convert(1e-9, invert(metric.noneToGiga)), 'noneToGiga')
-      .and.exactly(convert(1e-6, invert(metric.noneToMega)), 'noneToMega')
-      .and.exactly(convert(1e-3, invert(metric.noneToKilo)), 'noneToKilo')
-      .and.exactly(convert(0.01, invert(metric.noneToHecto)), 'noneToHecto')
-      .and.exactly(convert(0.1, invert(metric.noneToDeca)), 'noneToDeca')
-      .and.exactly(convert(10, invert(metric.noneToDeci)), 'noneToDeci')
-      .and.exactly(convert(100, invert(metric.noneToCenti)), 'noneToCenti')
-      .and.exactly(convert(1e3, invert(metric.noneToMilli)), 'noneToMilli')
-      .and.exactly(convert(1e6, invert(metric.noneToMicro)), 'noneToMicro')
-      .and.exactly(convert(1e9, invert(metric.noneToNano)), 'noneToNano')
-      .and.exactly(convert(1e12, invert(metric.noneToPico)), 'noneToPico')
-      .and.exactly(convert(1e15, invert(metric.noneToFemto)), 'noneToFemto')
-      .and.exactly(convert(1e18, invert(metric.noneToAtto)), 'noneToAtto')
-      .and.exactly(convert(1e21, invert(metric.noneToZepto)), 'noneToZepto')
-      .and.exactly(convert(1e24, invert(metric.noneToYocto)), 'noneToYocto');
+    (1).should.be.approximately(convert(1e-24, invert(metric.none_yotta)), 1e-15, 'none_yotta')
+      .and.approximately(convert(1e-21, invert(metric.none_zetta)), 1e-15, 'none_zetta')
+      .and.exactly(convert(1e-18, invert(metric.none_exa)), 'none_exa')
+      .and.exactly(convert(1e-15, invert(metric.none_peta)), 'none_peta')
+      .and.exactly(convert(1e-12, invert(metric.none_tera)), 'none_tera')
+      .and.exactly(convert(1e-9, invert(metric.none_giga)), 'none_giga')
+      .and.exactly(convert(1e-6, invert(metric.none_mega)), 'none_mega')
+      .and.exactly(convert(1e-3, invert(metric.none_kilo)), 'none_kilo')
+      .and.exactly(convert(0.01, invert(metric.none_hecto)), 'none_hecto')
+      .and.exactly(convert(0.1, invert(metric.none_deca)), 'none_deca')
+      .and.exactly(convert(10, invert(metric.none_deci)), 'none_deci')
+      .and.exactly(convert(100, invert(metric.none_centi)), 'none_centi')
+      .and.exactly(convert(1e3, invert(metric.none_milli)), 'none_milli')
+      .and.exactly(convert(1e6, invert(metric.none_micro)), 'none_micro')
+      .and.exactly(convert(1e9, invert(metric.none_nano)), 'none_nano')
+      .and.exactly(convert(1e12, invert(metric.none_pico)), 'none_pico')
+      .and.exactly(convert(1e15, invert(metric.none_femto)), 'none_femto')
+      .and.exactly(convert(1e18, invert(metric.none_atto)), 'none_atto')
+      .and.exactly(convert(1e21, invert(metric.none_zepto)), 'none_zepto')
+      .and.exactly(convert(1e24, invert(metric.none_yocto)), 'none_yocto');
 
-    (0).should.be.exactly(convert(0, metric.noneToYotta), 'noneToYotta')
-      .and.exactly(convert(0, metric.noneToZetta), 'noneToZetta')
-      .and.exactly(convert(0, metric.noneToExa), 'noneToExa')
-      .and.exactly(convert(0, metric.noneToPeta), 'noneToPeta')
-      .and.exactly(convert(0, metric.noneToTera), 'noneToTera')
-      .and.exactly(convert(0, metric.noneToGiga), 'noneToGiga')
-      .and.exactly(convert(0, metric.noneToMega), 'noneToMega')
-      .and.exactly(convert(0, metric.noneToKilo), 'noneToKilo')
-      .and.exactly(convert(0, metric.noneToHecto), 'noneToHecto')
-      .and.exactly(convert(0, metric.noneToDeca), 'noneToDeca')
-      .and.exactly(convert(0, metric.noneToDeci), 'noneToDeci')
-      .and.exactly(convert(0, metric.noneToCenti), 'noneToCenti')
-      .and.exactly(convert(0, metric.noneToMilli), 'noneToMilli')
-      .and.exactly(convert(0, metric.noneToMicro), 'noneToMicro')
-      .and.exactly(convert(0, metric.noneToNano), 'noneToNano')
-      .and.exactly(convert(0, metric.noneToPico), 'noneToPico')
-      .and.exactly(convert(0, metric.noneToFemto), 'noneToFemto')
-      .and.exactly(convert(0, metric.noneToAtto), 'noneToAtto')
-      .and.exactly(convert(0, metric.noneToZepto), 'noneToZepto')
-      .and.exactly(convert(0, metric.noneToYocto), 'noneToYocto');
+    (0).should.be.exactly(convert(0, metric.none_yotta), 'none_yotta')
+      .and.exactly(convert(0, metric.none_zetta), 'none_zetta')
+      .and.exactly(convert(0, metric.none_exa), 'none_exa')
+      .and.exactly(convert(0, metric.none_peta), 'none_peta')
+      .and.exactly(convert(0, metric.none_tera), 'none_tera')
+      .and.exactly(convert(0, metric.none_giga), 'none_giga')
+      .and.exactly(convert(0, metric.none_mega), 'none_mega')
+      .and.exactly(convert(0, metric.none_kilo), 'none_kilo')
+      .and.exactly(convert(0, metric.none_hecto), 'none_hecto')
+      .and.exactly(convert(0, metric.none_deca), 'none_deca')
+      .and.exactly(convert(0, metric.none_deci), 'none_deci')
+      .and.exactly(convert(0, metric.none_centi), 'none_centi')
+      .and.exactly(convert(0, metric.none_milli), 'none_milli')
+      .and.exactly(convert(0, metric.none_micro), 'none_micro')
+      .and.exactly(convert(0, metric.none_nano), 'none_nano')
+      .and.exactly(convert(0, metric.none_pico), 'none_pico')
+      .and.exactly(convert(0, metric.none_femto), 'none_femto')
+      .and.exactly(convert(0, metric.none_atto), 'none_atto')
+      .and.exactly(convert(0, metric.none_zepto), 'none_zepto')
+      .and.exactly(convert(0, metric.none_yocto), 'none_yocto');
   });
 });
